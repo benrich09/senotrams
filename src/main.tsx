603ec8632645e5b88a7daa1c64fb921ff56458b9
benrich09 <benrich205@gmail.com>
2025-10-07 18:10:14 +0300

    fresh code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { createRoot } from 'react-dom/client';  // Or whatever you're using (e.g., createRoot)
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+// Example render:
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
